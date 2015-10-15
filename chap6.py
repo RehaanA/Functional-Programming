71bcc49ac7f6591678675ddda5cf6913a2aac518
RehaanA <rehaan.advani@gmail.com>
2015-10-15 09:41:45 -0700

    chap6.py

---
+++ chap6.py
@@ -0,0 +1,9 @@
+def main():
+    print('+', '-', '-', '-', '-', '+', '-', '-', '-', '-', '+')
+    for i in range(4):
+        print("|")
+    print("+")
+    for i in range(4):
+        print("|")
+    print("+")
+main()
