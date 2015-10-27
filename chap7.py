5a0e605f7907851c17b2904a13d8897ebd879d3d
RehaanA <rehaan.advani@gmail.com>
2015-10-27 09:10:40 -0700

    Update chap7.py

--- chap7.py
+++ chap7.py
@@ -2,7 +2,19 @@
 #Chapter 7 Programming Assignments
 
 def proj7_1():
-    s1, s2, s3 = eval(input("Enter the three sides of a triangle separated by commas: "))
+    print("This program tells the you if you have a right triangle!")
+    
+    try:
+        s1, s2, s3 = eval(input("Enter the three sides of a triangle separated by commas: "))
+    except TypeError:
+        print("\nOops! Make sure you enter 3 numbers!")
+    except SyntaxError:
+        print("\nMake sure the numbers are separated by commas!")
+    except UnboundLocalError:
+        print("\nSomething went wrong!")
+    except:
+        print("\nSomething went wrong.")
+
 
     if s1 > s2 and s1 > s3:
         check = s2**2 + s3**2
@@ -31,3 +43,38 @@ def proj7_1():
         else:
             print("You don't have a right triangle.")
 proj7_1()
+
+def proj7_2():
+    print()
+    print("This program tests to see whether or not you have a valid triangle.")
+
+    try:
+        a, b, c = eval(input("Enter the three lengths of a triangle separated by commas: "))
+    except TypeError:
+        print("\nMake sure you only enter in numbers!")
+    except:
+        print("\nOops! Something went wrong.")
+
+    if a + b > c:
+        print("You have a valid triangle")
+    elif b + c > a:
+        print("You have a valid triangle")
+    elif c + a > b:
+        print("You have a valid triangle")
+    else:
+        print("You don't have a valid triangle")
+    
+proj7_2()
+
+def proj7_3():
+
+    try:
+        length = eval(input("Enter in the total length of your phone call in minutes: "))
+    except:
+        print("\nOops, something went wrong.")
+
+    cost = 0
+
+    if length == 2:
+        cost = 1.15
+        print("The total cost of your call is", cost)
