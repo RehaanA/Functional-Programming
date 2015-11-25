5b2475137e62537e4968a267ee4d51ce5080b9de
RehaanA <rehaan.advani@gmail.com>
2015-11-24 16:56:28 -0800

    Update chap9.py

--- chap9.py
+++ chap9.py
@@ -1,40 +1,18 @@
 #Rehaan Advani
 #Chapter 9 Programming Assignments
-#Period 2
 
 from random import *
 
-'''def proj9_1():
-    piArray = []
-    piSum = 0
-    h = 0
-    hArray = []
-    n = eval(input("Enter in the number of darts: "))
-    for i in range(10000):
-        x = 2 * random() - 1
-        y = 2 * random() - 1
-        if x**2 + y**2 <= 1:
-            h += 1
-            hArray.append(h)
-            
-        pi = (4 * len(hArray)) / n
-        piArray.append(pi)
-
-    for j in (piArray):
-        piSum += piArray[j]
-    averagePi = piSum / len(piArray)
-    print("The estimates pi is", averagePi)
-proj9_1()'''
-
 def proj9_1():
     n = eval(input("Enter in the number of darts: "))
-    x = 2 * random() - 1
-    y = 2 * random() - 1
     h = 0
-    for i in range(100000):
+    for i in range(n):
+        x = 2 * random() - 1
+        y = 2 * random() - 1
         if (x**2) + (y**2) <= 1:
             h += 1
 
     pi = 4 * h / n
-    print(pi)
+    print("The estimation of pi is", pi)
 proj9_1()
+
