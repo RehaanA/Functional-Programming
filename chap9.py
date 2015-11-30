573d84296eef3c6d49d83c018dcad8170ed12853
RehaanA <rehaan.advani@gmail.com>
2015-11-30 09:30:51 -0800

    Update chap9.py

--- chap9.py
+++ chap9.py
@@ -16,3 +16,27 @@ def proj9_1():
     print("The estimation of pi is", pi)
 proj9_1()
 
+def proj9_2():
+    numCoinFlips = eval(input("Enter in the number of times you want to flip a coin: "))
+    
+    stepsForward = 0
+    stepsBack = 0
+    totalSteps = 0
+    
+    for i in range(numCoinFlips):
+        randNum = randrange(0, 2)
+        if randNum == 0:
+            stepsBack += 1
+        else:
+            stepsForward += 1
+
+    totalSteps += stepsForward
+    totalSteps -= stepsBack
+
+    if totalSteps < 0:
+        print("You are {0} steps away from the starting point".format(abs(totalSteps)))
+    elif totalSteps == 0:
+        print("You are right where you started")
+    else:
+        print("you are {0} steps away from the starting point".format(totalSteps))
+proj9_2()
