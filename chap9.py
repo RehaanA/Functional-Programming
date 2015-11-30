f13f8654d26d7d79f3a708087f71df8b3d7abc11
RehaanA <rehaan.advani@gmail.com>
2015-11-30 09:56:27 -0800

    Update chap9.py

--- chap9.py
+++ chap9.py
@@ -40,3 +40,28 @@ def proj9_2():
     else:
         print("you are {0} steps away from the starting point".format(totalSteps))
 proj9_2()
+
+def proj9_3():
+    print()
+    print("This is a roulette program for $1 bets")
+    numSpins = eval(input("Enter the number of spins: "))
+    red = 0
+    black = 0
+    green = 0
+    total = 0
+
+    for i in range(numSpins):
+        randNum = randrange(0, 38)
+        if randNum >= 0 and randNum <= 17:
+            red += 1
+            total += 2
+        elif randNum >= 18 and randNum <= 35:
+            black += 1
+            total += 2
+        else:
+            green += 1
+            total += 35
+
+    print("The results were: {0} greens, {1} reds, and {2} blacks".format(green, red, black))
+
+proj9_3()
